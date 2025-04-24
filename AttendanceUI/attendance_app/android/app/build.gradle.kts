dab649373e010232a9c67c095f59856a415e9f7e
Triadi <triadideveloper@gmail.com>
2025-04-24 09:48:45 +0700

    feat: update structure

--- AttendanceUI/attendance_app/android/app/build.gradle.kts
+++ AttendanceUI/attendance_app/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.package.attendance_app"
+    namespace = "com.triadi.attendanceapp"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.package.attendance_app"
+        applicationId = "com.triadi.attendanceapp"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
